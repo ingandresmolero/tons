fbf59b0897f955efd6e97af7962e84a25e733fa9
Johnnn <thearmos97@gmail.com>
2022-07-08 09:16:40 -0400

    Primera Version del sistema, sin backend

--- index.php
+++ index.php
@@ -4,9 +4,70 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <title>ON SERVICE - SYSTEM</title>
 </head>
 <body>
-    <h1>AQUI VA EL SEGUIMIENTO</h1>
+    
+    <div class="container-fluid contenedor-main text-center">
+        <div class="header text-center">
+            <h1>Tracking Service</h1>
+            <h2>Titulo</h2>
+        </div>
+
+        <div class="container-xxl container-tables">
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">ODS</th>
+                        <th scope="col">Tecnico</th>
+                        <th scope="col">Equipo</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Fecha Ingreso</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </table>
+        </div>
+
+    </div>
+
+    <footer class="site-footer mt-5">
+        <div class="container-fluid px-5 footer-on">
+            <h3>Copyright 2022 © Todos los derechos reservados.<span> Diseñado por <a href="https://www.onserviceve.com/">ON SERVICE, C.A.</a></span></h3>
+        </div>
+    </footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
