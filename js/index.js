cc83fc3ffdfc9b0a136a195b7736c51123318734
Esteban Colmenares <estebancode91@gmail.com>
2022-07-12 14:02:51 -0400

    Agregando archivos no añadidos en el anterior commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function showMenu() {
+    document.querySelector('.header__menu').classList.toggle('active');
+    document.querySelector('.burgerList__img').classList.toggle('hide');
+    document.querySelector('.cancel__img').classList.toggle('show');
+}
