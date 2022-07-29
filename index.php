2676e3a8ed4bcfbda5e2f33db6b395b4476e9354
Esteban Augusto Colmenares Blanco <estebancode91@gmail.com>
2022-07-29 09:56:56 -0400

    Crear Registro

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ON SERVICE - SYSTEM</title>
     <link rel="stylesheet" href="./css/estilos.css">
     <link rel="stylesheet" href="./css/desktop.css" media="screen and (min-width:990px)">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <title>ON SERVICE - SYSTEM</title>
 </head>
 <body>
 
@@ -36,12 +37,10 @@
                     </a>
                 
 
-                
                     <a onclick="showMenu()" href="#" class="cancel__img">
-                        <img src="./css/img/cruz.png"  alt="cerrar menu para moviles">
+                        <i class="icon bi bi-x-lg"></i>
                     </a>
                 
-
             </div>
         </div>
     </header>
@@ -49,57 +48,68 @@
     <main>
         <div class="container">
             <div class="main__content">
-                <table class="table">
+
+                <button id="mostrarRegistro">Crear un nuevo registro</button>
+
+                <div class="main__formContainer hide" id="form"> 
+                    <div class="formTitle">
+                        <h2>REGISTRO</h2>
+                        <a href="" class="closeWindow"><i class="icon bi bi-x-lg"></i></a>
+                    </div>
+                    
+                    <form action="" method="POST" class="main__form main__form--flex main__form--registro">
+                        <label for="descripcion" class="labelDescripcion"><span>Descripción</span></label>
+                        <input type="text" name="" id="descripcion" class="" autofocus required value="">
+
+                        <label for="nroOrden" class="labelNroOrden"><span>Nro Orden</span></label>
+                        <input type="text" name="" id="nroOrden" class="" autofocus required value="">
+                        
+                        <label for="tipoEquipo" class="labelTipoEquipo"><span>Tipo de Equipo</span></label>
+                        <input type="text" name="" id="tipoEquipo" class="" autofocus required value="">
+                        
+                        <label for="fechaRecepcion" class="labelFechaRecepcion"><span>Fecha de Recepción</span></label>
+                        <input type="text" name="" id="fechaRecepcion" class="" autofocus required value="">
+                        
+                        <label for="lineaEquipo" class="labelLineaEquipo"><span>Línea de Equipo</span></label>
+                        <input type="text" name="" id="lineaEquipo" class="" autofocus required value="">
+                        
+                        <label for="situacion" class="labelSituacion"><span>Situación</span></label>
+                        <input type="text" name="" id="situacion" class="" autofocus required value="">
+                        
+                        <input class="btn" type="button" value="REGISTRAR">
+                    </form>
+                </div>
+                
+                <table class="table" id="table">
 
                     <thead>
                         <tr>
-                            <th>Asignado</th>
-                            <th>Presup. Aprobado</th>
-                            <th>Devolución</th>
-                            <th>Reparado</th>
-                            <th>Reincidencia</th>
+                            <th>Descripción</th>
+                            <th>Nro Orden</th>
+                            <th>Tipo de Equipo</th>
+                            <th>Fecha de Recepción</th>
+                            <th>Línea de Equipo</th>
+                            <th>Situación</th>
                         </tr>
                     </thead>
 
                     <tbody>
                           <tr>
-                                <td data-label="Asignado">Andres Molero</td>
-                                <td data-label="Presup. Aprobado">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, sapiente!</td>
-                                <td data-label="Devolución">Lorem ipsum dolor sit amet.</td>
-                                <td data-label="Reparado">Lorem, ipsum.</td>
-                                <td data-label="Reincidencia">Lorem ipsum, dolor sit amet consectetur adipisicing.</td>
+                                <td data-label="Descripción">Andres Molero</td>
+                                <td data-label="Nro Orden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, sapiente!</td>
+                                <td data-label="Tipo de Equipo">Lorem ipsum dolor sit amet.</td>
+                                <td data-label="Fecha de Recepción">Lorem, ipsum.</td>
+                                <td data-label="Línea de Equipo">Lorem ipsum, dolor sit amet consectetur adipisicing.</td>
+                                <td data-label="Situación">Lorem ipsum, dolor sit amet consectetur adipisicing.</td>
                           </tr>
                
                           <tr>
-                                <td data-label="Asignado">Andres Molero</td>
-                                <td data-label="Presup. Aprobado">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, sapiente!</td>
-                                <td data-label="Devolución">Lorem ipsum dolor sit amet.</td>
-                                <td data-label="Reparado">Lorem, ipsum.</td>
-                                <td data-label="Reincidencia">Lorem ipsum, dolor sit amet consectetur adipisicing.</td>
-                          </tr>
-               
-                          <tr>
-                                <td data-label="Asignado">Andres Molero</td>
-                                <td data-label="Presup. Aprobado">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, sapiente!</td>
-                                <td data-label="Devolución">Lorem ipsum dolor sit amet.</td>
-                                <td data-label="Reparado">Lorem, ipsum.</td>
-                                <td data-label="Reincidencia">Lorem ipsum, dolor sit amet consectetur adipisicing.</td>
-                          </tr>
-               
-                          <tr>
-                                <td data-label="Asignado">Andres Molero</td>
-                                <td data-label="Presup. Aprobado">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, sapiente!</td>
-                                <td data-label="Devolución">Lorem ipsum dolor sit amet.</td>
-                                <td data-label="Reparado">Lorem, ipsum.</td>
-                                <td data-label="Reincidencia">Lorem ipsum, dolor sit amet consectetur adipisicing.</td>
-                          </tr>
-               
-                          <tr>
-                                <td data-label="Asignado">Andres Molero</td>
-                                <td data-label="Presup. Aprobado">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, sapiente!</td>
-                                <td data-label="Devolución">Lorem ipsum dolor sit amet.</td>
-                                <td data-label="Reparado">Lorem, ipsum.</td>
-                                <td data-label="Reincidencia">Lorem ipsum, dolor sit amet consectetur adipisicing.</td>
+                                <td data-label="Descripción">Andres Molero</td>
+                                <td data-label="Nro Orden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, sapiente!</td>
+                                <td data-label="Tipo de Equipo">Lorem ipsum dolor sit amet.</td>
+                                <td data-label="Fecha de Recepción">Lorem, ipsum.</td>
+                                <td data-label="Línea de Equipo">Lorem ipsum, dolor sit amet consectetur adipisicing.</td>
+                                <td data-label="Situación">Lorem ipsum, dolor sit amet consectetur adipisicing.</td>
                           </tr>
                  
                     </tbody>
